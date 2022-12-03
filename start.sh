7de4ec88799231dcc55e786d2afba12fa79e430c
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-03 13:04:13 +0530

    ⚡️⚡️ successfully Changed server to KOYEB ⚡️⚡️

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
 cd /LazyUrlHunterrBOT
 pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting LazyUrlHunterrBOT.."
+echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
 python3 main.py
